018015ddd1baf956b6c4080ee61c3696517fadcd
dhakercoder <shubhamdhaker777@gmail.com>
2020-08-08 09:26:04 +0530

    adding node files

---
+++ Creating_a_Custom_Backend _Using_NodeJs/index.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const fs = require('fs')
+
+const hostname = '127.0.0.1';
+const port = 8000;
+
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('Hello World');
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
