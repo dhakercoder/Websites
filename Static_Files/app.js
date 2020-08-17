d3f6613432c285c4c66a50459a2a5bd5c4da68b1
dhakercoder <shubhamdhaker777@gmail.com>
2020-08-17 11:17:40 +0530

    project

--- Static_Files/app.js
+++ Static_Files/app.js
@@ -3,28 +3,22 @@ const path = require('path');
 const app = express();
 const port = 8000;
 
+// express stuff
 app.use('/static', express.static('static'))
 
-// set the tamplate engine as pug
+// pug stuff
+app.set('view engine','pug');// set the tamplate engine as pug
+app.set('views',path.join(__dirname,'views'));//  set the views directory
 
-app.set('view engine','pug')
 
-//  set the views directory
-app.set('views',path.join(__dirname,'views'))
 
+// end points
 
 app.get("/",(req, res)=>{
-    res.status(200).send(`This is home page of my first express application`);
+    res.status(200).render('index.pug')
 })
 
-app.get("/about",(req, res)=>{
-    res.send(`This is about page of my first express application`);
-})
-
-app.post("/about",(req, res)=>{
-    res.send(`This is post request in about page of my first express application`);
-})
-
+// start the port
 app.listen(port, ()=>{
     console.log(`The application was started successfully on port ${port}`);
 })
---
+++ pug_code/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8000;
+
+app.use('/static', express.static('static'))
+
+// set the tamplate engine as pug
+
+app.set('view engine','pug')
+
+//  set the views directory
+app.set('views',path.join(__dirname,'views'))
+
+
+// endpoint of our demo.pug
+
+app.get("/demo",(req, res)=>{
+    res.status(200).render('demo', { title: 'Hey shubham', message: 'Hello there! thankyou' })
+})
+
+app.get("/",(req, res)=>{
+    res.status(200).send(`This is home page of my first express application`);
+})
+
+app.get("/about",(req, res)=>{
+    res.send(`This is about page of my first express application`);
+})
+
+app.post("/about",(req, res)=>{
+    res.send(`This is post request in about page of my first express application`);
+})
+
+app.listen(port, ()=>{
+    console.log(`The application was started successfully on port ${port}`);
+})
+
+
+
+
